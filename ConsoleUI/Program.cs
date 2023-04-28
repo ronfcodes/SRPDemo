1e98064e5a3904c55f7c7dce0c1eb61d81ef2669
Ron <7442684+ronfcodes@users.noreply.github.com>
2023-04-27 20:10:57 -0400

    Initial commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleUI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to my application!");
+        Person user = new Person();
+
+        Console.WriteLine("What is your first name: ");
+        user.FirstName = Console.ReadLine();
+        Console.WriteLine("What is your last name: ");
+        user.LastName = Console.ReadLine();
+
+        if(string.IsNullOrWhiteSpace(user.FirstName))
+        {
+            Console.WriteLine("What is your first name: ");
+            Console.ReadLine();
+            return;
+        }
+        if (string.IsNullOrWhiteSpace(user.LastName))
+        {
+            Console.WriteLine("What is your last name: ");
+            Console.ReadLine();
+            return;
+        }
+
+        //Create a username for the person
+        Console.WriteLine($"Your username is { user.FirstName.Substring(0,1)}{user.LastName}");
+        Console.ReadLine();
+    }
+}
